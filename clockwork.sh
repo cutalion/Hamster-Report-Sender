103f0357a102731b2b9636085e876136d2774185
Alexander Glushkov <cutalion@gmail.com>
2011-08-13 14:12:56 +0300

    inital version of clockwork script
    clockwork script is for timelogs upload to clockwork

---
+++ clockwork.sh
@@ -0,0 +1,19 @@
+login_page=`curl $clockwork_url/login 2>/dev/null`
+
+auth_token=${login_page#*\"csrf-token\" content=\"}
+auth_token=${auth_token%%\"*}
+
+auth_param=${login_page#*\"csrf-param\" content=\"}
+auth_param=${auth_param%%\"*}
+
+cookies=tempfile
+login_query="$clockwork_url/user_sessions -F $auth_param=$auth_token -F user_session[email]=$email -F user_session[password]=$password"
+curl $login_query -c $cookies > /dev/null 2>&1
+
+
+grep user_credentials $cookies > /dev/null
+if [ $? == 0 ]; then
+  echo "Logged in! Yahoo!"
+else
+  echo "Can not login into clockwork :("
+fi
