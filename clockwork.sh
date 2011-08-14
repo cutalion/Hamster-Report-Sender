ae50e5e7949fd3d044a49b18a76ee80e99a497e1
Alexander Glushkov <cutalion@gmail.com>
2011-08-14 23:15:16 +0300

    clockwork script refactoring; post timelogs
    create fnuctions in utils for login, visit pages, getting csrf protection pair

--- clockwork.sh
+++ clockwork.sh
@@ -3,22 +3,17 @@
 
 include ./config/clockwork.conf
 
-login_page=`curl $clockwork_url/login 2>/dev/null`
-
-auth_token=${login_page#*\"csrf-token\" content=\"}
-auth_token=${auth_token%%\"*}
-
-auth_param=${login_page#*\"csrf-param\" content=\"}
-auth_param=${auth_param%%\"*}
-
-cookies=`tempfile`
-login_query="$clockwork_url/user_sessions -F $auth_param=$auth_token -F user_session[email]=$email -F user_session[password]=$password"
-curl $login_query -c $cookies > /dev/null 2>&1
-
-
-grep user_credentials $cookies > /dev/null
+login
 if [ $? == 0 ]; then
-  echo "Logged in! Yahoo!"
+  echo "Logged in."
 else
   echo "Can not login into clockwork :("
+  exit 1
 fi
+
+echo "Posting timelogs:"
+visit "$CLOCKWORK_URL/timelogs"
+post_timelog "2011-08-14" "2.5" "testing" "Project 1" && echo -n .
+
+echo ''
+echo "Done."
--- utils.sh
+++ utils.sh
@@ -4,3 +4,51 @@ include() {
     shift
   done
 }
+
+visit() {
+  local url=$1
+  shift
+
+  # echo "VISIT: curl $url "$@" -b $cookies -c $cookies"
+  current_page=`curl $url "$@" -b $cookies -c $cookies 2>/dev/null`
+}
+
+auth_param_token() {
+  local auth_token
+  local auth_param
+
+  auth_token=${1#*\"csrf-token\" content=\"}
+  auth_token=${auth_token%%\"*}
+
+  auth_param=${1#*\"csrf-param\" content=\"}
+  auth_param=${auth_param%%\"*}
+
+  echo "$auth_param=$auth_token"
+}
+
+login(){
+  visit $CLOCKWORK_URL/login
+
+  auth_pt=`auth_param_token "$current_page"`
+
+  cookies=`tempfile`
+  login_query="$CLOCKWORK_URL/user_sessions -F $auth_pt -F user_session[email]=$EMAIL -F user_session[password]=$PASSWORD"
+  curl $login_query -c $cookies > /dev/null 2>&1
+
+  grep user_credentials $cookies > /dev/null
+  if [ $? == 0 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+post_timelog(){
+  local date="$1"
+  local hours="$2"
+  local description="$3"
+  local project="$4"
+  local auth_pair=`auth_param_token "$current_page"`
+
+  visit "$CLOCKWORK_URL/timelogs" -F $auth_pair -F timelog[date]="$date" -F timelog[hours_input]="$hours" -F timelog[description]="$description" -F timelog[project_title]="$project"
+}
